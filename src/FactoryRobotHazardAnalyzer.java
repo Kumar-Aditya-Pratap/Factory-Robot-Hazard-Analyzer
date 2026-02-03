0f7cb22d56e606500f7224040ec5db433937bcf3
Kumar-Aditya-Pratap <128625213+Kumar-Aditya-Pratap@users.noreply.github.com>
2026-02-03 15:52:21 +0530

    [Aditya Pratap]UC2: Accept robot hazard inputs

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -1,6 +1,28 @@
+import java.util.*;
+/*
+* Version 2.0
+* Author:Kumar Aditya Pratap
+* Accept robot hazard inputs
+*/
+
 public class FactoryRobotHazardAnalyzer {
 
     public static void main(String[] args) {
         System.out.println("Factory Robot Hazard Analyzer");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Arm precision:");
+        double armPrecision = sc.nextDouble();
+
+        System.out.println("Enter Worker Density:");
+        int workerDensity = sc.nextInt();
+
+        System.out.println("Enter Machinery State:");
+        String machineryState = sc.next();
+
+        System.out.println("Arm Precision: " + armPrecision);
+        System.out.println("Worker Density: " + workerDensity);
+        System.out.println("Machinery State: " + machineryState);
+
+        sc.close();
     }
 }
